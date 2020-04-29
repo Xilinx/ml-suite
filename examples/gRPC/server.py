bbab58e485a272d00115519708c98c6d86d6f163
Tom Lou <louyu27@cs.washington.edu>
2020-04-28 18:31:47 -0700

    Try to fix writing to array

--- examples/gRPC/server.py
+++ examples/gRPC/server.py
@@ -39,7 +39,7 @@ def main():
 
     input = [mp.Array(ctypes.c_float, np.prod(shape)) for shape in input_shapes]
     for a, shape in zip(input, input_shapes):
-        a[:] = np.zeros(shape)
+        np.frombuffer(a.get_obj())[:] = np.zeros(shape)
     output = [mp.Array(ctypes.c_float, np.prod(shape)) for shape in input_shapes]
     input_dict = {name: shape for name, shape in zip(input_node_names, input)}
     output_dict = {name: shape for name, shape in zip(output_node_names, output)}
