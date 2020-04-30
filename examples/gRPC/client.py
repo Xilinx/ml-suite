e70a1a16d535694f37656162727fa7c48bc0773b
Tom Lou <louyu27@cs.washington.edu>
2020-04-29 21:20:55 -0700

    Client up client code

--- examples/gRPC/client.py
+++ examples/gRPC/client.py
@@ -8,32 +8,56 @@ import grpc
 import numpy as np
 import time
 
+# gRPC server info
+SERVER_ADDRESS = "localhost"
+SERVER_PORT = 5000
 
-def generate_requests():
-    try:
-        for _ in range(1000):
-            request = request_wrapper.dictToProto({"data": np.zeros((3, 224, 224), dtype=np.float32)})
-            yield request
-    except Exception as e:
-        import traceback
-        traceback.print_exc()
-        raise e
+# Number of dummy images to send
+N_DUMMY_IMAGES = 1000
 
 
-def start_client():
-    with grpc.insecure_channel('localhost:5000') as channel:
+def empty_image_generator(n):
+    '''
+    Generate empty images
+
+    n: number of images
+    '''
+    for _ in range(n):
+        request = {"data": np.zeros((3, 224, 224), dtype=np.float32)}
+        request = request_wrapper.dictToProto(request)
+        yield request
+
+
+def dummy_client(n, print_interval=50):
+    '''
+    Start a dummy client
+
+    n: number of images to send
+    print_interval: print a number after this number of images is done
+    '''
+    print("Dummy client sending {n} images...".format(n=n))
+
+    start_time = time.time()
+    # Connect to server
+    with grpc.insecure_channel('{address}:{port}'.format(address=SERVER_ADDRESS,
+                                                         port=SERVER_PORT)) as channel:
         stub = inference_server_pb2_grpc.InferenceStub(channel)
 
-        responses = stub.Inference(generate_requests())
+        # Make a call
+        responses = stub.Inference(empty_image_generator(n))
+
+        # Get responses
         for i, response in enumerate(responses):
-            if i % 50 == 0:
+            if i % print_interval == 0:
                 print(i)
             # print(request_wrapper.protoToDict(response,
             #                                   {"loss3_classifier/Reshape_output": (1024)}))
+    total_time = time.time() - start_time
+    print("{n} images in {time} seconds ({speed} images per second)"
+          .format(n=n,
+                  time=total_time,
+                  speed=float(n) / total_time))
 
 
 if __name__ == '__main__':
-    request = request_wrapper.dictToProto({"data": np.zeros((3, 224, 224))})
-    start_time = time.time()
-    start_client()
-    print(time.time() - start_time)
+    dummy_client(N_DUMMY_IMAGES)
