20008fd6325d6794291fbcd8fa5b63f1ba685cf2
Tom Lou <louyu27@cs.washington.edu>
2020-04-29 21:11:15 -0700

    Fixed simple client

--- examples/gRPC/client.py
+++ examples/gRPC/client.py
@@ -6,23 +6,34 @@ import request_wrapper
 
 import grpc
 import numpy as np
+import time
 
 
 def generate_requests():
-    for _ in range(3):
-        request = request_wrapper.dictToProto({"input": np.zeros((3, 224, 224))})
-        yield request
+    try:
+        for _ in range(1000):
+            request = request_wrapper.dictToProto({"data": np.zeros((3, 224, 224), dtype=np.float32)})
+            yield request
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        raise e
 
 
 def start_client():
-    with grpc.insecure_channel('ec2-54-245-214-208.us-west-2.compute.amazonaws.com:5000') as channel:
+    with grpc.insecure_channel('localhost:5000') as channel:
         stub = inference_server_pb2_grpc.InferenceStub(channel)
 
         responses = stub.Inference(generate_requests())
-        for response in responses:
-            print(request_wrapper.protoToDict(response))
+        for i, response in enumerate(responses):
+            if i % 50 == 0:
+                print(i)
+            # print(request_wrapper.protoToDict(response,
+            #                                   {"loss3_classifier/Reshape_output": (1024)}))
 
 
 if __name__ == '__main__':
-    request = request_wrapper.dictToProto({"input": np.zeros((3, 224, 224))})
+    request = request_wrapper.dictToProto({"data": np.zeros((3, 224, 224))})
+    start_time = time.time()
     start_client()
+    print(time.time() - start_time)
